c368d14390010fcbf12081719c3ac2cacd0a615f
Saurabh Parkar <59107123+nexuseli18@users.noreply.github.com>
2024-06-12 13:02:37 -0400

    Update fl_enb.py

--- POWDER/fl-xApp/flapp-enb/fl_enb.py
+++ POWDER/fl-xApp/flapp-enb/fl_enb.py
@@ -5,35 +5,54 @@ from ricxappframe.xapp_frame import RMRXapp, rmr
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import tensorflow as tf
-from ricsdl.syncstorage import SyncStorage
-
 
 # Setting device MEID
-meid = os.environ['MEID'] = bytes('enb01', 'utf-8')
-
-# Loading Data (Dummy Data for Development)
-mnist = tf.keras.datasets.mnist
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-x_train, x_test = x_train / 255.0, x_test / 255.0
-
-
+meid = os.environ['MEID'] 
+meid = bytes(meid, 'utf-8')
 # Setting Params
 weights = 0
-epochs = 10
+epochs = 0
 my_ns = 'flapp'
 model = None
 
+def load_data():
+    '''Loads data from csibins'''
+    path = 'Dataset/x-310-3/nuc3/train/'
+    x3_mag = np.fromfile(f'Dataset/x-310-3/nuc3/train/nuc3_mag.bin', dtype=np.float32)
+    x3_phase = np.fromfile(f'Dataset/x-310-3/nuc3/train/nuc3_phase.bin', dtype=np.float32)
+
+    x4_mag = np.fromfile(f'Dataset/x-310-3/nuc4/train/nuc4_mag.bin', dtype=np.float32)
+    x4_phase = np.fromfile(f'Dataset/x-310-3/nuc4/train/nuc4_phase.bin', dtype=np.float32)
+
+    slice_size_3 = len(x3_mag)%52
+    slice_size_4 = len(x4_mag)%52
+
+    if slice_size_3 != 0:
+        x3_mag = x3_mag[:-slice_size_3]
+        x3_phase = x3_phase[:-slice_size_3]
+    if slice_size_4 != 0:
+        x4_mag = x4_mag[:-slice_size_4]
+        x4_phase = x4_phase[:-slice_size_4]
+    _X_mag = np.concatenate((x3_mag, x4_mag))
+    _X_phase = np.concatenate((x3_phase, x4_phase))
+    _y = np.concatenate((np.zeros(len(x3_mag)), np.ones(len(x4_mag))))
+
+    index = np.random.permutation(len(_X_mag))
+    X_mag = _X_mag[index]
+    X_phase = _X_phase[index]
+    y = _y[index]
+
+
+
+    return X_mag, X_phase, y
+
+X_mag, X_phase, y = load_data()
 
 def post_init(self: RMRXapp):
     '''POST Init Function, executes on running app sends serialized model object to RIC'''
     global epochs, model, meid, weights
-    print(f'eNB Post Init: {self.healthcheck()}\neNBs Connected {self.sdl._sdl} \nsending model \n')
-    serialized_model = self.sdl.get(my_ns, 'model')
-    model = tf.keras.models.deserialize(serialized_model)
-    weights = self.sdl.get(my_ns, 'weights')
-    print(f'Model Loaded from SDL\n')
-    model.summary()
-    payload = json.dumps({'ACK': 'Model Loaded'}).encode()
+    print(f'eNB Post Init: {self.healthcheck()}\neNBs Connected')
+    payload = json.dumps({'REQ': 'Model'}).encode()
     if model:
         sbuf = rmr.rmr_alloc_msg(vctx=self._mrc, size=len(payload),payload=payload,
                                 gen_transaction_id=True, meid=meid, mtype=1000)
@@ -49,41 +68,43 @@ def default_handler(self, summary, sbuf):
     print('defh')
 
 # 2000 sends data received ack and begins Training
-def get_weights(self, summary, sbuf):
+def get_model(self, summary, sbuf):
     '''Loading in the aggregatted weights received for RIC and continues training'''
-    global weights, bias, epochs, model
-    print('getdata')
+    global weights, epochs, model
+    print(f'Weights Received\n{summary[rmr.RMR_MS_PAYLOAD]}\n Begin Training')
+
     jpay = json.loads(summary[rmr.RMR_MS_PAYLOAD])
-    print(f'Pre\nsum: {jpay}\nsbuf: {sbuf}')
+    _model = jpay['model']
+    model = tf.keras.utils.deserialize_keras_object(_model)
     weights = jpay['weights']
-    weights = [np.array(i) for i in weights]
-    model.set_weights(weights)
+    if weights != 0:
+        weights = [np.array(i) for i in weights]
+        model.set_weights(weights)
+    epochs = jpay['epochs']
+    print(f'Epochs Remaining: {epochs}\nTraining Model\n')
     train(self)
+    
 
 
 def train_enb(self, summary, sbuf):
     '''Initial Train Call'''
     global epochs, weights, model
     print(f'train initiate for eNB\n payload: {summary.keys()}')
-    weights = self.sdl.get(my_ns, 'weights')
+    jpay = json.loads(summary[rmr.RMR_MS_PAYLOAD])
+    weights = jpay['weights']
     
-    epochs = self.sdl.get(my_ns, 'epochs')
     if epochs > 0:
         if weights != 0:
             weights = [np.array(i) for i in weights]
             model.set_weights(weights)
-            print(f'Epochs Remaining: {epochs}\nTraining Model\n')
-            train(self)
-        elif weights == 0:
-            weights = [np.array(i) for i in weights]
-            model.set_weights(weights)
-            print(f'Epochs Remaining: {epochs}\nTraining Model\n')
-            train(self)
+        print(f'Epochs Remaining: {epochs}\nTraining Model\n')
+        epochs -= 1
+        train(self)
 
 def train(xapp: RMRXapp):
     '''trains model on remaining epochs and sends weights back to ric for averaging'''
-    global weights, model, x_train, x_test, y_train, y_test, epochs, meid
-    model.fit(x_train, y_train, batch_size=32, epochs=1, validation_split=0.2)
+    global weights, model, X_mag, X_phase, y, epochs, meid
+    model.fit([X_mag,X_phase], y, batch_size=32, epochs=1, validation_split=0.2)
     weights = model.get_weights()
     weights = [i.tolist() for i in weights]
     payload = json.dumps({'weights': weights}).encode()
@@ -95,17 +116,12 @@ def train(xapp: RMRXapp):
             xapp.rmr_free(sbuf)
             break
     
-    
-    
-    
-    # data = json.dumps({'weights': weights, 'epochs': epochs})
-    # xapp.rmr_send(data.encode(), 1001)
 
 
 # xapp initiation
 xapp = RMRXapp(default_handler=default_handler,
                post_init=post_init, use_fake_sdl=True, rmr_port=4561)
-xapp.register_callback(get_weights, 2000)
+xapp.register_callback(get_model, 2000)
 xapp.register_callback(train_enb, 2001)
 # xapp.register_callback(model_update,2002)
 xapp.run()
